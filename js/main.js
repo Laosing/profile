52bfe701aef99ae353e0efe4e02adf80fc1c6f30
Andrew <andrew.rattana@gmail.com>
2014-12-09 21:07:27 -0500

    removing jquery template for handlebards

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 var Portfolio = function() {
 
   var self = this,
-      config = {
-        checkout: $('#checkout'),
-        main: $('.main')
-      };
+      checkout = $('#checkout'),
+      main = $('.main')
 
   this.init = function() {
-    config.checkout.click(function() {
-      config.main.toggleClass('active');
+    checkout.click(function() {
+      main.toggleClass('active');
     });
 
     this.timeline();
@@ -21,26 +19,6 @@ var Portfolio = function() {
       .done(function(data) {
         $.each(data.work, function(key, value) {
 
-          $('#timeline').loadTemplate($('#timeline-template'), {
-            id: 'timeline' + key,
-            title: value.timeline.title,
-            desc: value.timeline.description
-          }, {append: true, afterInsert: function($ele) {
-            $ele.click(function() {
-              var el = this.id.match(/\d+/)[0];
-              console.log(+el);
-              $('#' + el).toggleClass('active');
-            })
-          }});
-
-          $('#work-container').loadTemplate($('#work-template'), {
-            id: key,
-            img: 'img/' + value.modal.image,
-            alt: value.modal.title,
-            title: value.modal.title,
-            desc: value.modal.description,
-            link: value.modal.link
-          }, {append: true});
         });
       });
   }
